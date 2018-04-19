dcbfc39b7360e075d85a224d10fd6ac3574829d3
Bjarte Bore <bjarte.bore@olavstoppen.no>
2018-04-19 18:42:07 +0200

    added the possibility to change provisioning profile uuid and signining identity from task

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const run = () => {
     const developmentTeam = tl.getInput('DevelopmentTeam', true);
     const targetName = tl.getInput('TargetName', true);
     const ensureProvisioningStyleManual = tl.getBoolInput('ensureProvisioningStyleManual');
+    const signingIdentity = tl.getInput('signingIdentity', false);
+    const provisioningProfileUUID = tl.getInput('provisioningProfileUUID', false);
     // Paths
     const workingDir = tl.getPathInput('cwd');
     const fileMatches = tl.findMatch(
@@ -26,7 +28,7 @@ const run = () => {
       project.getTargets().forEach((target) => {
 
         if (target.name !== targetName) {
-          tl.debug(`Target: ${targetName} not matching configured target: ${targetName}`);
+          tl.debug(`Target: ${target.name} not matching configured target: ${targetName}`);
           return;
         }
 
@@ -51,12 +53,46 @@ const run = () => {
           'DEVELOPMENT_TEAM',
           developmentTeam);
 
+        if (signingIdentity !== "") {
+          tl.debug(`Configuring CODE_SIGN_IDENTITY: ${signingIdentity} for target: ${target.name}`)
+          project.configuration.setUserDefinedTargetConfiguration(
+            target.name,
+            configuration,
+            'CODE_SIGN_IDENTITY',
+            signingIdentity);
+        }
+
+        if(ensureProvisioningStyleManual) {
+          tl.debug(`Configuring CODE_SIGN_STYLE: Manual for target: ${target.name}`)
+          project.configuration.setUserDefinedTargetConfiguration(
+            target.name,
+            configuration,
+            'CODE_SIGN_STYLE',
+            'Manual');
+        }
+
+        if (provisioningProfileUUID !== "") {
+          tl.debug(`Configuring PROVISIONING_PROFILE: ${provisioningProfileUUID} for target: ${target.name}`)
+          project.configuration.setUserDefinedTargetConfiguration(
+            target.name,
+            configuration,
+            'PROVISIONING_PROFILE',
+            provisioningProfileUUID);
+
+          tl.debug(`Configuring PROVISIONING_PROFILE_SPECIFIER: '' for target: ${target.name}`)
+          project.configuration.setUserDefinedTargetConfiguration(
+            target.name,
+            configuration,
+            'PROVISIONING_PROFILE_SPECIFIER',
+            '');
+
+        }
 
       });
 
       if(ensureProvisioningStyleManual) {
-        tl.debug("Ensures that ProvisionStyle is set to Manual");
-        project.myProj.addTargetAttribute("ProvisioningStyle", "Manual");
+        tl.debug('Ensures that ProvisionStyle is set to Manual');
+        project.myProj.addTargetAttribute('ProvisioningStyle', 'Manual');
       }
 
       project.save();
